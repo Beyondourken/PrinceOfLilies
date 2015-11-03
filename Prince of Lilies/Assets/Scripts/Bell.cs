c63e491bd69d45880ec31d17d454cf785f792a4e
Hilary Blower <HilaryBlower@gmail.com>
2015-11-03 16:18:39 +0000

    Added custom house scene with bell ring and reaction

---
+++ Prince of Lilies/Assets/Scripts/Bell.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bell : MonoBehaviour {
+	
+	private GameObject player;              // Reference to the player.
+	public GameObject setTargetOn;
+
+	
+	
+
+	
+	void OnTriggerStay (Collider col)
+	{
+		// If the colliding gameobject is the player...
+		if (col.CompareTag("Player")) {
+			// ... and the switch button is pressed...
+			if (Input.GetButton ("Submit")) {
+				
+				RingBell ();
+
+			}
+		}
+	}
+	
+	
+	void RingBell ()
+	{
+
+		// Play allarm bell audio clip.
+		GetComponent<AudioSource>().Play();
+		setTargetOn.SendMessage("SetTarget", transform);
+	
+	}
+
+}
+
+
+		
+
